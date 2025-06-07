65e0f8b7979c86c6d51668736c2d72041d8feb23
Andrés Felipe Medina <37091017+andresFMedina@users.noreply.github.com>
2025-06-07 08:14:05 -0500

    Added functionality navigation and style to buttons

--- Source/FrontendUI/Public/Widgets/Widget_ActivatableBase.h
+++ Source/FrontendUI/Public/Widgets/Widget_ActivatableBase.h
@@ -6,12 +6,19 @@
 #include "CommonActivatableWidget.h"
 #include "Widget_ActivatableBase.generated.h"
 
-/**
- * 
- */
+class AFrontendPlayerController;
+
 UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class FRONTENDUI_API UWidget_ActivatableBase : public UCommonActivatableWidget
 {
 	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintPure)
+	AFrontendPlayerController* GetOwningFrontendPlayerController();
+
+private:
+	TWeakObjectPtr<AFrontendPlayerController> CachedOwningPC;
+
 	
 };
